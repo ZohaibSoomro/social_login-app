75f6d200d1741862be8f436158d1b96588da4cda
zohaibsoomro006 <zohaibsoomro006@gmail.com>
2022-10-15 02:56:45 +0500

    added info screen

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -11,7 +11,7 @@ class InfoPage extends StatelessWidget {
         title: const Text('Info screen'),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           CircleAvatar(
             radius: 50,
@@ -20,14 +20,26 @@ class InfoPage extends StatelessWidget {
                   'https://tse2.mm.bing.net/th?id=OIP.fBYQQAMqYDFxOh5dWobzLgHaFj&pid=Api&P=0',
             ),
           ),
+          SizedBox(width: MediaQuery.of(context).size.width),
           const SizedBox(height: 30),
-          Text('${user.displayName}'),
+          Text(
+            '${user.displayName}',
+            style: buildTextStyle(),
+          ),
           const SizedBox(height: 10),
-          Text('${user.email}'),
-          const SizedBox(height: 10),
-          Text('${user.phoneNumber}'),
+          Text(
+            '${user.email}',
+            style: buildTextStyle(),
+          ),
         ],
       ),
+    );
+  }
+
+  buildTextStyle() {
+    return const TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
     );
   }
 }
